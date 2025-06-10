4bbee6946a56078f4d47ebcdb8a783815b3213d9
souhail <souhailkhalmadani11@gmail.com>
2025-06-10 09:33:57 +0100

    created the read function for minio

--- database/__init__.py
+++ database/__init__.py
@@ -28,10 +28,10 @@ def make_buckets(bucket_list: list = ["webscraping", "traitement"]):
             print(f"Bucket '{bucket_name}' déjà existant.")
 
 
-def save_json(
+def save_to_minio(
     bucket_name="webscraping",
     object_name="default",
-    file_path="data_extraction/default.json",
+    file_path="/app/database/default.json",
     content_type="application/json",
 ):
     try:
@@ -40,3 +40,19 @@ def save_json(
         print(f" Upload : {object_name}")
     except S3Error as err:
         print(f" Erreur : {object_name} → {err}")
+
+
+def read_json(
+    bucket_name="webscraping",
+    object_name="default",
+    file_path="/app/data_extraction/test_download.json",
+    content_type="application/json",
+):
+    try:
+        client = start_client()
+        json_file = client.fget_object(
+            bucket_name, object_name, file_path, content_type
+        )
+        return json_file
+    except Exception as e:
+        print(f"Can't download object from object storage {e}")
