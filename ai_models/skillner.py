6bd62e1d71936afafa7ad4b4fe1e772109769868
souhail <souhailkhalmadani11@gmail.com>
2025-06-12 16:48:35 +0100

    changed the docker environment file to .docker.env, added annotation function and global skill NER function to ai_model/skillner.py

--- ai_models/skillner.py
+++ ai_models/skillner.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import spacy
 
@@ -9,46 +10,90 @@ from skillNer.general_params import SKILL_DB
 from skillNer.skill_extractor_class import SkillExtractor
 from spacy.matcher import PhraseMatcher
 
-# init params of skill extractor
-nlp = spacy.load("en_core_web_lg")
-# init skill extractor
-skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
+from database import save_to_minio
 
 
-job_offers = json.load(
-    open(
-        r"C:\Users\khalm\Documents\Stage2025\Job_analytics\data_extraction\scraping_output\offres_emploi_rekrute.json",
-        "r",
-        encoding="utf-8",
+def annotate_text(filename="offres_emploi_rekrute.json") -> list:
+    """This functions uses spacy's NLP and  skillner's skill extractor and a custom skill database to annotate text.
+
+    This text can displayed using skillextractor's describe and display methods.
+
+    Parameters
+    ----------
+    filename:
+      the name of the json file to extract text from and then annotate
+    """
+    # init params of skill extractor
+    nlp = spacy.load("en_core_web_lg")
+    file_path = os.path.join(os.getcwd(), r"data_extraction\scraping_output", filename)
+    # init skill extractor
+    skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
+
+    job_offers = json.load(
+        open(
+            file_path,
+            "r",
+            encoding="utf-8",
+        )
     )
-)
 
-annotations = []
+    annotations = []
 
-for job_offer in job_offers:
-    try:
-        annotations.append(skill_extractor.annotate(job_offer["description"]))
-    except Exception as e:
-        print(f"Exception during the annotation phase: {e}")
-        continue
+    for job_offer in job_offers:
+        try:
+            annotations.append(
+                skill_extractor.annotate(
+                    job_offer["description"] + job_offer["competences"]
+                )
+            )
 
-print(annotations)
-for i in range(10):
-    job_offer = annotations[i]
-    print(f"---Job offer n°{i + 1}---")
-    for full_match in job_offer["results"]["full_matches"]:
-        key = full_match["skill_id"]
-        print(
-            f"Full match skill {key}---->",
-            SKILL_DB[key]["skill_name"],
-            "//",
-            SKILL_DB[key]["skill_type"],
-        )
-    for ngram_score in job_offer["results"]["ngram_scored"]:
-        key = ngram_score["skill_id"]
-        print(
-            f"Approximate match skill {key}---->",
-            SKILL_DB[key]["skill_name"],
-            "//",
-            SKILL_DB[key]["skill_type"],
-        )
+        except Exception as e:
+            print(f"Exception during the annotation phase: {e}")
+            continue
+    return annotations
+
+
+def extract_skills(filename="offres_emploi_rekrute.json") -> list:
+    """Given the filename of a json file, this function will do NER on the skills present in the file's text.
+
+    Parameters
+    ---------
+    filename:
+        The name of the json file
+    """
+    # First we annotate the text
+    annotations = annotate_text(filename=filename)
+    ner_data = []
+    # During this step we go through the annotations and match the skill id's from SKILL_DB with the skill names
+    for i in range(len(annotations)):
+        job_offer = annotations[i]
+        skills = {}
+
+        print(f"---Job offer n°{i + 1}---")
+        for full_match in job_offer["results"]["full_matches"]:
+            key = full_match["skill_id"]
+            """print(
+                f"Full match skill {key}---->",
+                SKILL_DB[key]["skill_name"],
+                "//",
+                SKILL_DB[key]["skill_type"],
+            )"""
+            skills[SKILL_DB[key]["skill_name"]] = SKILL_DB[key]["skill_type"]
+        for ngram_score in job_offer["results"]["ngram_scored"]:
+            key = ngram_score["skill_id"]
+            """print(
+                f"Approximate match skill {key}---->",
+                SKILL_DB[key]["skill_name"],
+                "//",
+                SKILL_DB[key]["skill_type"],
+            )"""
+            skills[SKILL_DB[key]["skill_name"]] = SKILL_DB[key]["skill_type"]
+        ner_data.append(skills)
+    ner_filename = "NER_" + filename
+    with open(ner_filename, "w", encoding="utf-8") as js_file:
+        json.dump(ner_data, js_file, ensure_ascii=False, indent=4)
+    save_to_minio(file_path=ner_filename)
+    return ner_data
+
+
+extract_skills()
